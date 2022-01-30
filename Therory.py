0232f51866ab9a954306baee50e21ba9f575bbe2
vinod521 <vinodkumar.it380@gmail.com>
2022-01-30 18:29:02 +0530

    first commit

---
+++ Therory.py
@@ -0,0 +1,74 @@
+                                 #Learn Python
+from importlib.machinery import WindowsRegistryFinder
+from sqlite3 import ProgrammingError
+from unicodedata import digit
+from xml.etree.ElementInclude import LimitedRecursiveIncludeError
+
+
+-------------------------------------------------------------------------------------------------------------
+                             #Python Introduction
+-------------------------------------------------------------------------------------------------------------
+#Python was developed by Guido Van Rossam in 1989 while working at national research institute at Netherlands.
+#But officially python was made available to public in 1991.
+#The official date of birth for python is : Feb 20th 1991.
+#Python is recommended as first programming language for beginners.
+
+--------------------------------------------------------------------------------------------------------------
+
+#Guido Developed python language by taking almost all programming features from different languages.
+
+1.#Functional Programming Features from C.
+2.#Objeect Oriented Programming Features from C++.
+3.#Scripting Language Features from Perl and Shell Script.
+4.#Modular Programming Features from Modula-3.
+
+#//Most of syntax in python derived from C and ABC language.
+
+---------------------------------------------------------------------------------------------------------------
+#Usage of Python Language
+
+1. For developing Desktop Application.
+2. For developing Web Application.
+3. For Developing database Application.
+4. For Network Programming.
+5. For developing Games.
+6. For Data Analysis Application.
+7. For Machine Learning.
+8. For developing AI application.
+
+---------------------------------------------------------------------------------------------------------------
+
+# How to different Python other language
+
+1. #No Semi- Colon (;)
+
+#Python uses new line to complete a command, as opposed to other programming language .Which often semicolons or parentheses.
+
+2. #No Curly Braces ({})
+
+#Python relies on indentation, using whitespace,to define scope;
+#such as the scope of loops, function and classes. Other programming language often use curly - brackets for this purpose.
+
+
+#First Chapter# Variables In Python #
+
+#Variables are containers for storing data values.
+#Variables do not need to be declared with any particular type,any can even change type after they have been set.
+
+1. Camel case - nameofStudent
+2. Pascal case - NameofStudent
+3. Snake case - name_of_student
+
+
+#Second Chapter# Identifiers in Python
+
+# A name in python program is called identifier. It can be class name or function name or variable name.
+
+1.Alphabets, 0-9, underscor(_), no specail symbol allow ($)
+2.Not start with digit
+3.Cannot use reserved Words.
+4.No length Limit.
+5.Case sensitive.
+
+
+
